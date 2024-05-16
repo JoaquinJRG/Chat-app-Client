f2ad43217148d1e244edc2d368525d716398c8e8
Joaquin <joaquinjrg01@gmail.com>
2024-05-16 12:27:09 +0200

    blur modal

--- src/components/sections/Chats/Modal.jsx
+++ src/components/sections/Chats/Modal.jsx
@@ -34,7 +34,7 @@ export function Modal({ setShowModal, updateChatsList }) {
   }
 
   return (
-    <div onClick={clickOutside} className="w-full lg:w-11/12 h-full flex justify-center items-center fixed bg-black z-20 bg-opacity-40 overflow-y-auto shadow-md">
+    <div onClick={clickOutside} className="w-full lg:w-11/12 h-full flex justify-center items-center fixed backdrop-blur-sm z-20 overflow-y-auto shadow-md">
       <div ref={modalRef} className="w-80 p-4 bg-white rounded-md z-40 fade-in-up">
         <header className="flex items-center justify-between mb-8">
           <h3 className="text-2xl">Nuevo Chat</h3>
