0b11d9f0760e1a93f9c1f24152c3555ad9ea3763
Joaquin <joaquinjrg01@gmail.com>
2024-05-17 11:06:35 +0200

    add delete user modal

---
+++ src/components/sections/Settings/DeleteModal.jsx
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+import { XIcon } from "../../icons/XIcon";
+
+export function DeleteModal({ setShowModal }) {
+
+  const idUser1 = JSON.parse(localStorage.getItem("userData")).id;
+  const modalRef = useRef();
+
+  const clickOutside = (e) => {
+    if (modalRef.current && !modalRef.current.contains(e.target)) {
+      setShowModal(false);
+    }
+  }
+
+  return (
+    <div onClick={clickOutside} className="w-full lg:w-11/12 h-full flex justify-center items-center fixed backdrop-blur-sm z-20 overflow-y-auto shadow-md">
+      <div ref={modalRef} className="w-80 p-4 bg-white rounded-md z-40 fade-in-up border">
+        <header className="flex items-center justify-between mb-8">
+          <h3 className="text-2xl">Eliminar la cuenta</h3>
+          <button onClick={() => setShowModal(false)} className="hover:text-indigo-500 transition-colors"><XIcon /></button>
+        </header>
+        <main>
+          <div className="flex item-center justify-between">
+            <button onClick={() => setShowModal(false)} className="bg-white-500 text-indigo-500 border border-indigo-500 rounded-lg px-3 py-2 transition-all hover:-translate-y-1 hover:shadow-lg">Cancelar</button>
+            <button className="bg-indigo-500 text-white rounded-lg px-3 py-2 transition-all hover:-translate-y-1 hover:shadow-lg">Aceptar</button>
+          </div>
+        </main>
+      </div>
+    </div>
+  )
+}
--- src/components/sections/Settings/Settings.jsx
+++ src/components/sections/Settings/Settings.jsx
@@ -5,10 +5,12 @@ import { UserIcon } from "../../icons/UserIcon";
 import { ProfilePic } from "../Home/ProfilePic";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { DeleteModal } from "./DeleteModal";
 
 export function Settings() {
 
   const userData = JSON.parse(localStorage.getItem("userData"))
+  const [showDeleteModal, setDeleteModal] = useState(false);
 
   const copyId = () => {
     navigator.clipboard.writeText(userData.id);
@@ -39,55 +41,71 @@ export function Settings() {
   }
 
   return (
-    <main className="p-4 mt-10 lg:mt-0">
+    <main className="mt-10 lg:mt-0">
+      {showDeleteModal && <DeleteModal setShowModal={setDeleteModal} />}
       <ToastContainer />
-      <h1 className="text-4xl text-indigo-500 mb-5">Configuración</h1>
-      <section className="fade-in-up">
-        <h2 className="text-3xl mb-5">Perfil</h2>
-        <div className="flex flex-col lg:flex-row gap-10">
-          <div>
-            <ProfilePic />
+      <div className="p-4">
+        <h1 className="text-4xl text-indigo-500 mb-5">Configuración</h1>
+        <section className="fade-in-up">
+          <h2 className="text-3xl mb-5">Perfil</h2>
+          <div className="flex flex-col lg:flex-row gap-10">
+            <div>
+              <ProfilePic />
+            </div>
+            <div className="flex flex-col items-center text-base lg:text-lg">
+              <ul className="flex flex-col gap-2">
+                <li className="flex items-center justify-start gap-2">
+                  <IdIcon />
+                  <span className="font-bold">Id:</span>
+                  {userData.id}
+                  <span title="Copiar" onClick={copyId} className="hover:text-violet-500 cursor-pointer">
+                    <CopyIcon />
+                  </span>
+                </li>
+                <li className="flex items-center justify-start gap-2">
+                  <UserIcon />
+                  <span className="font-bold">Nombre:</span>
+                  {userData.nombre}
+                </li>
+                <li className="flex items-center justify-start gap-2">
+                  <MailIcon />
+                  <span className="font-bold">Correo:</span>
+                  {userData.correo}
+                </li>
+              </ul>
+              <form onSubmit={sendImg} className="flex items-center justify-start gap-2 mt-4">
+                <label className="flex items-center gap-2 bg-indigo-500 text-white rounded-lg px-3 py-2 transition-all hover:-translate-y-1 hover:shadow-lg cursor-pointer" htmlFor="image">
+                  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-camera">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M5 7h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2" />
+                    <path d="M9 13a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                  </svg>
+                  Seleccionar Imagen
+                </label>
+                <input type="file" id="image" name="image" accept="image/*" className="hidden" />
+                <input
+                  type="submit"
+                  value="Aceptar"
+                  className="bg-indigo-500 text-white rounded-lg px-3 py-2 transition-all hover:-translate-y-1 hover:shadow-lg cursor-pointer"
+                />
+              </form>
+            </div>
           </div>
-          <div className="flex flex-col items-center text-base lg:text-lg">
-            <ul className="flex flex-col gap-2">
-              <li className="flex items-center justify-start gap-2">
-                <IdIcon />
-                <span className="font-bold">Id:</span>
-                {userData.id}
-                <span title="Copiar" onClick={copyId} className="hover:text-violet-500 cursor-pointer">
-                  <CopyIcon />
-                </span>
-              </li>
-              <li className="flex items-center justify-start gap-2">
-                <UserIcon />
-                <span className="font-bold">Nombre:</span>
-                {userData.nombre}
-              </li>
-              <li className="flex items-center justify-start gap-2">
-                <MailIcon />
-                <span className="font-bold">Correo:</span>
-                {userData.correo}
-              </li>
-            </ul>
-            <form onSubmit={sendImg} className="flex items-center justify-start gap-2 mt-4 ">
-              <label className="flex items-center gap-2 bg-indigo-500 text-white rounded-lg px-3 py-2 transition-all hover:-translate-y-1 hover:shadow-lg cursor-pointer" htmlFor="image">
-                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-camera">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M5 7h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2" />
-                  <path d="M9 13a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
-                </svg>
-                Seleccionar Imagen
-              </label>
-              <input type="file" id="image" name="image" accept="image/*" className="hidden" />
-              <input
-                type="submit"
-                value="Aceptar"
-                className="bg-indigo-500 text-white rounded-lg px-3 py-2 transition-all hover:-translate-y-1 hover:shadow-lg cursor-pointer"
-              />
-            </form>
-          </div>
-        </div>
-      </section>
+        </section>
+        <section className="fade-in-up">
+          <h2 className="text-3xl mb-5 mt-10">Tema</h2>
+          <button>Cambiar a tema oscuro</button>
+        </section>
+        <section className="fade-in-up">
+          <h2 className="text-3xl mb-5 mt-10">Cuenta</h2>
+          <button
+            onClick={() => setDeleteModal(true)}
+            className="bg-white px-3 py-2 border border-red-500 text-red-500 rounded-lg transition-all hover:-translate-y-1 hover:shadow-lg hover:bg-red-500 hover:text-white cursor-pointer"
+          >
+            Eliminar Cuenta
+          </button>
+        </section>
+      </div>
     </main>
   )
 }
